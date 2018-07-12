515936e104d96fb4c645a8c8de933c855411583e
mlinvilleengler <mlinvilleengler@paypal.com>
2018-07-12 01:30:28 -0700

    feat(landscape): handle resize events

--- dots/src/Components/Landscape.js
+++ dots/src/Components/Landscape.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react'
 import './Landscape.css'
 import uuid from 'uuid/v4'
+import debounce from 'lodash.debounce'
 
 class Landscape extends Component {
   constructor () {
     super()
     this.state = {
       hills: new Array(25).fill(0).map(() => {
-        const hillOptions = this.getRandomHillOptions()
-
-        const hill = (
-          <div
-            key={uuid()}
-            role='presentation'
-            className='hill'
-            style={{
-              ...hillOptions
-            }}
-          />
-        )
-        return hill
+        return this.getRandomHillOptions()
       })
     }
+  }
+
+  componentDidMount () {
+    const resizeEvent = window.addEventListener(
+      'resize',
+      debounce(() => this.handleResize(), 100)
+    )
+
+    this.setState({ resizeEvent })
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener(this.state.resizeEvent)
+  }
+
+  handleResize = () => {
+    this.setState(prevState => ({
+      hills: prevState.hills.map(hill => {
+        const newHill = { ...hill }
+        const bottomRatio = this.getBottomRatio()
+        newHill.bottom = newHill.size * bottomRatio + 'vh'
+        return newHill
+      })
+    }))
   }
 
   getRandomHillOptions = () => {
@@ -34,31 +47,50 @@ class Landscape extends Component {
     const backgroundColor = `rgb(${colors.join()})`
     const borderRadius = Math.floor(Math.random() * 7 + 2)
     const opacity = Math.random() + 0.75
-    const innerWidth = window.innerWidth
-    let bottomStep = -0.5
-    if (innerWidth > 700) {
-      bottomStep = -0.6
-    }
-    if (innerWidth > 1000) {
-      bottomStep = -0.75
-    }
+    const bottomRatio = this.getBottomRatio()
 
     return {
+      size,
       zIndex,
       backgroundColor,
       opacity,
+      key: uuid(),
       borderRadius: borderRadius + '%',
       left: left + 'vw',
-      bottom: size * bottomStep + 'vh',
+      bottom: size * bottomRatio + 'vh',
       width: size + 'vw',
       height: size * 0.75 + 'vh'
     }
   }
 
+  getBottomRatio = () => {
+    let innerWidth = window.innerWidth
+    let bottomRatio = -0.5
+    if (innerWidth > 1000) {
+      bottomRatio = -0.75
+    } else if (innerWidth > 700) {
+      bottomRatio = -0.6
+    }
+    return bottomRatio
+  }
+
+  renderHills = () => {
+    return this.state.hills.map(hill => (
+      <div
+        key={hill.key}
+        role='presentation'
+        className='hill'
+        style={{
+          ...hill
+        }}
+      />
+    ))
+  }
+
   render () {
     return (
       <React.Fragment>
-        {this.state.hills}
+        {this.renderHills()}
       </React.Fragment>
     )
   }
